64be36a1c1f08e0399e051b7a5bd459aa7b141e3
AbstractGeek <AbstractGeek@users.noreply.github.com>
2025-05-14 02:57:19 +0000

    Deploying to gh-pages from @ AbstractGeek/ag-folio@f22303df5fad4bb7a39b47ded0e12958e5832cd3 🚀

--- assets/js/cronitor-analytics-setup.js
+++ assets/js/cronitor-analytics-setup.js
@@ -3,4 +3,4 @@ window.cronitor =
   function () {
     (window.cronitor.q = window.cronitor.q || []).push(arguments);
   };
-cronitor("config", { clientKey: "" });
+cronitor("config", { clientKey: "025dee7b06610ea42663c60962701347" });
